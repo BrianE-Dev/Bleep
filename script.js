cf1448e5d644319ad7e5401efd9100428c8c2c93
BrianE-Dev <chukwuemekankama1@gmail.com>
2025-09-29 01:06:30 +0100

    adjustments

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ console.log("bleeper")
     function newLocation(){
         console.log("Bleeper Logistics")
 
-const maxWeight = 10
-const cost = 10000
-var extraFee = 2500
-var destinationZone = "Lagos";
-var extraWeight = 1500 //Charge for weight exceeding maxWeight
+const maxWeight = 10;
+const cost = 10000;
+let extraFee, destinationZone, extraWeight, totalAmount;
+    extraFee = 2500
+    destinationZone = "Lagos";
+    extraWeight = 1500 //Charge for weight exceeding maxWeight
+    totalAmount = cost + extraFee;
 
-    let sum;
-    sum = cost + extraFee;
-    let totalAmount;
-        totalAmount = sum;
-        const currentLoc = document.getElementById("currentLocation").value;
-        const kg = document.getElementById("kg").value;
+    const currentLoc = document.getElementById("currentLocation").value;
+    const kg = document.getElementById("kg").value;
     const zone = document.getElementById("zone").value;
     const express = document.getElementById("express").checked;
 
@@ -23,7 +21,7 @@ var extraWeight = 1500 //Charge for weight exceeding maxWeight
     console.log("Your Location:", currentLoc);
     console.log("Destination:", zone);
     console.log("Express", express)
-        if (kg <= maxWeight && zone === destinationZone) console.log("The cost is " + (cost)) 
+        if (kg <= maxWeight && zone === destinationZone) console.log("The Base cost is " + (cost)) 
         if (zone !== destinationZone) console.log("The cost is " + (cost) + " + extra shipping fee of" + (extraFee))
             if (kg <=maxWeight && zone === destinationZone) console.log(" The Total Amount without express = " + (cost))
             if (kg > maxWeight && zone === destinationZone) console.log("Weight limit exceeded: " + (cost) + " + exceeded weight charge of " + (extraWeight) + ". The Total Amount without express is " + (cost + extraWeight))
